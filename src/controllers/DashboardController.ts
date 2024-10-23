3881a3f200a28c477aae8ce7e378971ebf764391
YanPuty <yanputy123@gmail.com>
2024-10-23 20:18:39 +0700

    Update API Dashboard

--- src/controllers/DashboardController.ts
+++ src/controllers/DashboardController.ts
@@ -4,27 +4,53 @@ import moment from 'moment-timezone';
 import { ITransaction } from 'src/models/Transaction';
 import { DashboardService } from 'src/services';
 
-import { ContextRequest, Controller, GET } from '../../packages';
+import { BadRequestError, ContextRequest, Controller, GET } from '../../packages';
 
 @Controller('/dashboards')
 @injectable()
 export class DashboardController {
-
   @inject('DashboardService')
-  dashboardSv!: DashboardService
+  dashboardSv!: DashboardService;
 
-  @GET('/v1/revenue/today')
-  async getTotalEarningToday(
-    @ContextRequest request: Request<any, any, ITransaction>,
-  ) {
-    // Define timezone as UTC+7 (Asia/Phnom_Penh)
+  @GET('/v1/revenue')
+  async getTotalEarningToday(@ContextRequest request: Request<any, any, ITransaction>) {
     const timezone = 'Asia/Phnom_Penh';
-    const { machine } = request.query;
-    // Get the start and end of the day in UTC+7
-    const startOfTodayUTC7 = moment.tz(timezone).startOf('day').utc().toDate(); // Convert to UTC
-    const endOfTodayUTC7 = moment.tz(timezone).endOf('day').utc().toDate();     // Convert to UTC
+    const { machine, from, to } = request.query;
 
-    const response = await this.dashboardSv.getTotalRevenueByDateRange(startOfTodayUTC7, endOfTodayUTC7, machine as string);
+    // Get the start and end of the day in UTC+7
+    let startOfTodayUTC7 = moment.tz(timezone).startOf('day').utc().toDate(); // Convert to UTC
+    let endOfTodayUTC7 = moment.tz(timezone).endOf('day').utc().toDate(); // Convert to UTC
+
+    // Adjust start and end dates if 'from' and 'to' parameters are provided
+    if (from) {
+      startOfTodayUTC7 = moment
+        .tz(from as string, 'DD-MM-YYYY', timezone)
+        .startOf('day')
+        .utc()
+        .toDate(); // Convert to UTC
+
+      if (!moment(startOfTodayUTC7).isValid()) {
+        throw new BadRequestError('Invalid Date Format');
+      }
+    }
+
+    if (to) {
+      endOfTodayUTC7 = moment
+        .tz(to as string, 'DD-MM-YYYY', timezone)
+        .endOf('day')
+        .utc()
+        .toDate(); // Convert to UTC
+
+      if (!moment(endOfTodayUTC7).isValid()) {
+        throw new BadRequestError('Invalid Date Format');
+      }
+    }
+
+    const response = await this.dashboardSv.getTotalRevenueByDateRange(
+      startOfTodayUTC7,
+      endOfTodayUTC7,
+      machine as string,
+    );
     return response;
   }
 }
